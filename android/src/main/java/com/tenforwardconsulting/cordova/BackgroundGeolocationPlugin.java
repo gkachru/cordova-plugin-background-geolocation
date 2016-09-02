00567b6b45406ec11c61d4d2c7e67ff7265dbf39
Gautam Kachru <gkachru@users.noreply.github.com>
2016-09-02 06:36:43 +0530

    Fixed missing imports

--- android/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
+++ android/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
@@ -37,6 +37,8 @@ import android.content.pm.PackageManager;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
